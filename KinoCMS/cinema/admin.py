7cf2c4257f1a96e2f4635b8576889d769abccc87
MaksymPashkisvkyi <maximajin@gmail.com>
2022-08-09 19:28:39 +0300

    Add list displays to admin panel

--- KinoCMS/cinema/admin.py
+++ KinoCMS/cinema/admin.py
@@ -4,34 +4,41 @@ from .models import CinemaModel, SEOModel, GalleryModel, HallModel, FilmModel, S
 
 @admin.register(SEOModel)
 class SEOAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'URL', 'title', 'keywords', 'description']
+    list_display_links = ['title']
 
 
 @admin.register(GalleryModel)
 class GalleryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'image']
+    list_display_links = ['name']
 
 
 @admin.register(CinemaModel)
 class CinemaAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'description', 'condition']
+    list_display_links = ['name']
 
 
 @admin.register(HallModel)
 class HallAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'description', 'hall_scheme', 'cinema']
+    list_display_links = ['name']
 
 
 @admin.register(FilmModel)
 class FilmAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'description', 'URL', 'three_D', 'two_D', 'IMAX']
+    list_display_links = ['name']
 
 
 @admin.register(SessionModel)
 class SessionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'film', 'hall', 'price', 'time', 'three_D', 'DBOX', 'VIP']
+    list_display_links = ['film']
 
 
 @admin.register(TicketModel)
 class TicketAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'session', 'user', 'seat', 'reservation']
+    list_display_links = ['session']
