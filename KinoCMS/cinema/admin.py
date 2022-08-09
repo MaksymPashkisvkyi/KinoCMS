97d5a7e572c7027b2f2600463b192b94f9b2db33
MaksymPashkisvkyi <maximajin@gmail.com>
2022-08-09 13:58:30 +0300

    Registation models in admin

--- KinoCMS/cinema/admin.py
+++ KinoCMS/cinema/admin.py
@@ -1,17 +1,79 @@
 from django.contrib import admin
-from .models import CinemaModel, SEOModel, GalleryModel
-
-
-@admin.register(CinemaModel)
-class CinemaAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
+from .models import CinemaModel, SEOModel, GalleryModel, HallModel, FilmModel, SessionModel, BannerConfigModel, \
+    PagesModel, TicketModel, BackgroundBannerModel, MainPageModel, BannerMainPageModel, NewsPromoModel, \
+    BannerNewsPromoModel, ContactsModel
 
 
 @admin.register(SEOModel)
 class SEOAdmin(admin.ModelAdmin):
-    list_display = ('URL',)
+    pass
 
 
 @admin.register(GalleryModel)
 class GalleryAdmin(admin.ModelAdmin):
-    list_display = ('image',)
+    pass
+
+
+@admin.register(CinemaModel)
+class CinemaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(HallModel)
+class HallAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FilmModel)
+class FilmAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SessionModel)
+class SessionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TicketModel)
+class TicketAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MainPageModel)
+class MainPageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PagesModel)
+class PagesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ContactsModel)
+class ContactsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(NewsPromoModel)
+class NewsPromoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BannerConfigModel)
+class BannerConfigAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BackgroundBannerModel)
+class BackgroundBannerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BannerMainPageModel)
+class BannerMainPageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BannerNewsPromoModel)
+class BannerNewsPromoAdmin(admin.ModelAdmin):
+    pass
