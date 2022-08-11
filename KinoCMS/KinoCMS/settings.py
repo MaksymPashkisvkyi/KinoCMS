158e66c99e3fd595bbf9535f1d3cde77c55759ff
MaksymPashkisvkyi <maximajin@gmail.com>
2022-08-11 11:53:09 +0300

    Add AdminLTE to project

--- KinoCMS/KinoCMS/settings.py
+++ KinoCMS/KinoCMS/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'user.apps.UserConfig',
     'pages.apps.PagesConfig',
     'banners.apps.BannersConfig',
+    'admin_lte.apps.AdminLteConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,10 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_FILES_DIRS = [
-    os.path.join(BASE_DIR, 'KinoCMS/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
