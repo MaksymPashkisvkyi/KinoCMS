736668adecf1023df19df3841139fbfc2df1279a
MaksymPashkisvkyi <maximajin@gmail.com>
2022-08-11 19:15:06 +0300

    add footer to base template

--- KinoCMS/pages/views.py
+++ KinoCMS/pages/views.py
@@ -3,9 +3,8 @@ from django.views.generic.base import TemplateView
 
 
 class MainPageView(TemplateView):
-    template_name = "cinema/main.html"
+    template_name = "pages/main.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        messages.info(self.request, "hello http://example.com")
         return context
