404805530d436db1f84af0d93d3ecbebe46d9033
MaksymPashkisvkyi <maximajin@gmail.com>
2022-08-17 17:16:17 +0300

    add templatetags to pages app

--- KinoCMS/pages/templatetags/pages_tags.py
+++ KinoCMS/pages/templatetags/pages_tags.py
@@ -3,13 +3,13 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('pages/navbar.html')
+@register.inclusion_tag('pages/base/navbar.html')
 def navbar():
     menu = []
     return {'menu': menu}
 
 
-@register.inclusion_tag('pages/social_media_icons.html')
+@register.inclusion_tag('pages/base/social_media_icons.html')
 def social_media_icons():
     icons = []
     return {'icons': icons}
