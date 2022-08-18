899e334895a91522809a124623dd71654a5323a1
MaksymPashkisvkyi <maximajin@gmail.com>
2022-08-18 22:24:09 +0300

    add url and views for init pages: about, advertisement, cafe, contacts, mobile_apps, news

--- KinoCMS/pages/urls.py
+++ KinoCMS/pages/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
 
-from .views import MainPageView
+from .views import MainPageView, PosterView, SoonView, ScheduleView, CinemasView, PromoView, ContactsView, CafeView, \
+    MobileAppsView, AdvertisementView, AboutView, NewsView
 
 urlpatterns = [
-    path('', MainPageView.as_view(), name='main')
+    path('', MainPageView.as_view(), name='main'),
+    path('poster/', PosterView.as_view(), name='poster'),
+    path('soon/', SoonView.as_view(), name='soon'),
+    path('schedule/', ScheduleView.as_view(), name='schedule'),
+    path('cinemas/', CinemasView.as_view(), name='cinemas'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('cafe/', CafeView.as_view(), name='cafe'),
+    path('mobile_apps/', MobileAppsView.as_view(), name='mobile_apps'),
+    path('advertisement/', AdvertisementView.as_view(), name='advertisement'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('promo/', PromoView.as_view(), name='promo'),
+    path('news/', NewsView.as_view(), name='news'),
 ]
