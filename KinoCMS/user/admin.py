88a66dacf827defd619cfc7ccbefba552bd693d7
MaksymPashkisvkyi <maximajin@gmail.com>
2022-08-18 15:14:11 +0300

    delete admin features for default admin panel

--- KinoCMS/user/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserModel
-
-
-@admin.register(UserModel)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'surname', 'nickname', 'email', 'address', 'gender', 'phone', 'date', 'city']
-    list_display_links = ['name']
