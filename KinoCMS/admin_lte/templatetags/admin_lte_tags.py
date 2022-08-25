ba6a01935cbe3c90ae8d5026f542f545af768efd
MaksymPashkisvkyi <maximajin@gmail.com>
2022-08-25 11:29:49 +0300

    refactor sidebar in base

--- KinoCMS/admin_lte/templatetags/admin_lte_tags.py
+++ KinoCMS/admin_lte/templatetags/admin_lte_tags.py
@@ -5,5 +5,32 @@ register = template.Library()
 
 @register.inclusion_tag('admin_lte/base/table_buttons.html')
 def table_buttons():
-    context = []
-    return {'context': context}
+    buttons = [
+        {
+            'class': 'edit fa fa-edit text-decoration-none text-reset',
+            'title': 'Edit'
+        },
+        {
+            'class': 'delete fa fa-trash text-decoration-none text-reset',
+            'title': 'Delete'
+        }
+    ]
+    return {'buttons': buttons}
+
+
+@register.inclusion_tag('admin_lte/base/sidebar_menu.html')
+def show_side_menu():
+    menu = [
+        {'title': "Статистика", 'url': "admin_statistic"},
+        {'title': "Баннера/Слайдеры", 'url': "admin_banner"},
+        {'title': "Фильмы", 'url': "admin_film"},
+        {'title': "Кинотеатры", 'url': "admin_cinema"},
+        {'title': "Новости#", 'url': "admin_statistic"},
+        {'title': "Акции#", 'url': "admin_statistic"},
+        {'title': "Страницы#", 'url': "admin_statistic"},
+        {'title': "Пользователи", 'url': "admin_user"},
+        {'title': "Рассылка#", 'url': "admin_statistic"},
+        {'title': "SEO", 'url': "admin_seo"},
+        {'title': "Галерея", 'url': "admin_gallery"}
+    ]
+    return {'menu': menu}
